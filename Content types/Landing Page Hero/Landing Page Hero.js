c32bc1443e5dd1de5b7d7be2470e89279d57a66d
tfleischauer <tfleischauer@gmail.com>
2025-10-23 17:47:46 -0700

    Initial commit: backup of existing work

---
+++ Content types/Landing Page Hero/Landing Page Hero.js
@@ -0,0 +1,39 @@
+<section class="hero hero--landing text-margin-reset hero--img<t4 type="content" name="BG Video URL" output="selective-output" format=" hero--video" />">
+
+    <div class="hero__media">
+        <t4 type="content" name="BG Video URL" output="selective-output" process-format="true" format="<div class=&quot;autoplay-video-container&quot;>
+            <video class=&quot;video&quot; aria-labelledby=&quot;hero-video--button-toggle&quot; id=&quot;hero&quot; loop muted playsinline>
+                <source src=&quot;<t4 type=&quot;content&quot; name=&quot;BG Video URL&quot; output=&quot;normal&quot; />&quot; type=&quot;video/mp4&quot;>
+            </video>
+            <button type=&quot;button&quot; class=&quot; video-play-button&quot; data-button-open-text=&quot;&quot;
+                data-button-close-text=&quot;&quot; data-button-enable-at=&quot;0&quot; data-button-disable-at=&quot;-1 &quot;
+                data-button-open-class=&quot;video-playing&quot;
+                data-button-open-class-element=&quot;.autoplay-video-container&quot; aria-live=&quot;polite&quot;
+                aria-label=&quot;Pause&quot; id=&quot;hero-video--button-toggle&quot; aria-controls=&quot;hero&quot;
+                data-toggle-type=&quot;toggle&quot; aria-pressed=&quot;false&quot;>
+            </button>
+        </div>" />
+        <img loading="eager" sizes="100vw" src="<t4 type="content" name="BG Image" output="normal" formatter="V10/image/pxl-crop" cdn="true" pxl-filter-id="4" />" srcset="
+            <t4 type="content" name="BG Image" output="normal" formatter="V10/image/pxl-crop" cdn="true" pxl-filter-id="4" /> 500w,
+            <t4 type="content" name="BG Image" output="normal" formatter="V10/image/pxl-crop" cdn="true" pxl-filter-id="3" /> 768w,
+            <t4 type="content" name="BG Image" output="normal" formatter="V10/image/pxl-crop" cdn="true" pxl-filter-id="2" /> 1600w,
+            <t4 type="content" name="BG Image" output="normal" formatter="V10/image/pxl-crop" cdn="true" pxl-filter-id="1" /> 1900w" alt="<t4 type="content" name="BG Image" output="normal" formatter="text/alt" />">
+    </div>
+
+
+    <div class="hero__overlay" aria-hidden="true"></div>
+    <div class="hero__content">
+        <h1><t4 type="content" name="Title" output="normal" modifiers="striptags,htmlentities" /></h1>
+        <div class="global-spacing--3x">
+            <p><t4 type="content" name="Description" output="normal" modifiers="striptags,htmlentities" /></p>
+        </div>
+
+        <div class="global-spacing--3x">
+            <t4 type="content" name="Show Breadcrumbs" output="selective-output" modifiers="nav_sections" process-format="true" format="<t4 type=&quot;navigation&quot; name=&quot;Breadcrumbs&quot; id=&quot;955&quot; />" />
+        </div>
+
+        <t4 type="content" name="CTA - Title" output="selective-output" process-format="true" format="<div class=&quot;global-spacing--3x&quot;>
+            <a href=&quot;<t4 type=&quot;content&quot; name=&quot;CTA - Internal Link&quot; output=&quot;linkurl&quot; modifiers=&quot;nav_sections&quot; /><t4 type=&quot;content&quot; name=&quot;CTA - External Link&quot; output=&quot;normal&quot; />&quot; class=&quot;btn&quot;><t4 type=&quot;content&quot; name=&quot;CTA - Title&quot; output=&quot;normal&quot; modifiers=&quot;striptags,htmlentities&quot; /></a>
+        </div>" />
+    </div>
+</section>
